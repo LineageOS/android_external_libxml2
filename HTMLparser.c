12ec5e842296157adbdbfcd04bd318310bf6d879
Nick Wellnhofer <wellnhofer@aevum.de>
2017-06-17 15:59:13 +0200

    Make HTML parser functions take const pointers
    The 'cur' parameter of htmlParseDoc and htmlSAXParseDoc should be
    'const xmlChar *'.
    
    Fixes bug 770650.
    
    Change-Id: I245c7e27af700c0454537816d4af1bf9e92abef2

--- HTMLparser.c
+++ HTMLparser.c
@@ -6081,7 +6081,8 @@ htmlCreatePushParserCtxt(htmlSAXHandlerPtr sax, void *user_data,
  */
 
 htmlDocPtr
-htmlSAXParseDoc(xmlChar *cur, const char *encoding, htmlSAXHandlerPtr sax, void *userData) {
+htmlSAXParseDoc(const xmlChar *cur, const char *encoding,
+                htmlSAXHandlerPtr sax, void *userData) {
     htmlDocPtr ret;
     htmlParserCtxtPtr ctxt;
 
@@ -6120,7 +6121,7 @@ htmlSAXParseDoc(xmlChar *cur, const char *encoding, htmlSAXHandlerPtr sax, void 
  */
 
 htmlDocPtr
-htmlParseDoc(xmlChar *cur, const char *encoding) {
+htmlParseDoc(const xmlChar *cur, const char *encoding) {
     return(htmlSAXParseDoc(cur, encoding, NULL, NULL));
 }
 
